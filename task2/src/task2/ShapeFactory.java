f8b5c1992772eba043fc8c8c98cd1332946a68de
Mushhood Ahmed Qazi <116354052+Mushhoodqazi@users.noreply.github.com>
2024-04-21 20:07:46 +0500

    Update ShapeFactory.java

--- task2/src/task2/ShapeFactory.java
+++ task2/src/task2/ShapeFactory.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package task2;
 
-/**
- *
- * @author hp
- */
 public class ShapeFactory {
-    
+    public static Shape createShape(String shapeType) {
+        if (shapeType == null) {
+            return null;
+        }
+        if (shapeType.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+        } else if (shapeType.equalsIgnoreCase("SQUARE")) {
+            return new Square();
+        } else if (shapeType.equalsIgnoreCase("TRIANGLE")) {
+            return new Triangle();
+        }
+        return null;
+    }
 }
